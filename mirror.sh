62747eb3910f9800897f6a665399e31a2ec20f11
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-03-05 07:25:30 +0100

    exit codes

--- mirror.sh
+++ mirror.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-git clone https://${access_token}@github.com/minetest-mirrors/${github_repo} gh_repo
+git clone https://${access_token}@github.com/minetest-mirrors/${github_repo} gh_repo || exit -1
 cd gh_repo
-git pull ${source_repo} ${branch}
-git push origin ${branch}
+git pull ${source_repo} ${branch} || exit -1
+git push origin ${branch} || exit -1
