d3e4c794586ba818d34f9a7a833d6a6ea5057fe2
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-03-06 07:10:52 +0100

    more lightweight commit compare

--- mirror.sh
+++ mirror.sh
@@ -1,6 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
-git clone https://${access_token}@github.com/minetest-mirrors/${github_repo} gh_repo || exit -1
+# full github url
+GITHUB_REPO_URL=https://${access_token}@github.com/minetest-mirrors/${github_repo}
+
+# fetch commit-checksums
+LOCAL_SHA=$(git ls-remote ${GITHUB_REPO_URL} | cut -f1)
+REMOTE_SHA=$(git ls-remote ${source_repo} | cut -f1)
+
+# exit if commits are equal
+test "${LOCAL_SHA}" == "${REMOTE_SHA}" && exit 0
+
+# clone, pull and push
+git clone -b ${branch} ${GITHUB_REPO_URL} gh_repo || exit -1
 cd gh_repo
 git pull ${source_repo} ${branch} || exit -1
 git push origin ${branch} || exit -1
