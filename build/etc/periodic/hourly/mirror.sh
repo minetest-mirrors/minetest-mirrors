cbb8aaf72579615444cb3b46e0f74b1dc53170bc
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-04-15 20:05:18 +0200

    port some code to new mirror script

--- build/etc/periodic/hourly/mirror.sh
+++ build/etc/periodic/hourly/mirror.sh
@@ -1,9 +1,29 @@
 #!/bin/sh
+set -e
 
-repos=$(cat /repos.json | jq '.[] | "remote=\(.remote) repo=\(.repo)"')
+# working dir
+cd /git_cache
 
-cat /repos.json | jq -M -r '.[] | .remote, .repo' |\
-while read -r remote; read -r repo; do
-    echo "Cfg: $remote / $repo"
-    # TODO
+# read repos.json and parse fields
+cat /repos.json | jq -M -r '.[] | .remote, .repo, .branch' |\
+while read -r remote; read -r repo; read -r branch; do
+    # default branch: master
+    test "$branch" == "null" && branch=master
+
+    echo "Checking repo ${repo} (remote: ${remote}) on branch ${branch}"
+
+    # full github url
+    GITHUB_REPO_URL="https://github.com/minetest-mirrors/${repo}"
+
+    # initial clone if the .git dir does not exist
+    test -d "$repo/.git" || git clone $remote $repo
+
+    # fetch commit-checksums
+    LOCAL_SHA=$(git ls-remote "${GITHUB_REPO_URL}" ${branch} | cut -f1)
+    REMOTE_SHA=$(git ls-remote "${remote}" ${branch} | head -n1 | cut -f1)
+
+    # abort if no changes found
+    test "${LOCAL_SHA}" == "${REMOTE_SHA}" && continue
+
+    echo "Changes found"
 done
