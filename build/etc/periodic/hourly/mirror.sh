6279191f4f55a18299a1af42493b01669eb20388
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-04-16 19:10:43 +0200

    sync all tags and branches

--- build/etc/periodic/hourly/mirror.sh
+++ build/etc/periodic/hourly/mirror.sh
@@ -8,35 +8,25 @@ ACCESS_TOKEN=$(cat /access_token)
 cd /git_cache
 
 # read repos.json and parse fields
-cat /repos.json | jq -M -r '.[] | .remote, .repo, .branch' |\
-while read -r remote; read -r repo; read -r branch; do
-    # default branch: master
-    test "$branch" == "null" && branch=master
-
-    echo "Checking repo ${repo} (remote: ${remote}) on branch ${branch}"
+cat /repos.json | jq -M -r '.[] | .remote, .repo' |\
+while read -r remote; read -r repo; do
+    echo "Checking repo ${repo} (remote: ${remote})"
 
     # full github url
     GITHUB_REPO_URL="https://${ACCESS_TOKEN}@github.com/minetest-mirrors/${repo}"
 
     # initial clone if the .git dir does not exist
-    test -d "$repo/.git" || git clone $remote $repo
+    test -d "$repo" || git clone --mirror $remote $repo
 
-    # fetch commit-checksums
-    LOCAL_SHA=$(git ls-remote "${GITHUB_REPO_URL}" ${branch} | cut -f1)
-    REMOTE_SHA=$(git ls-remote "${remote}" ${branch} | head -n1 | cut -f1)
-
-    # abort if no changes found
-    test "${LOCAL_SHA}" == "${REMOTE_SHA}" && continue
-
-    echo "Changes found"
     # change to repository directory
     cd ${repo}
 
     # pull latest changes
-    git pull origin ${branch}
+    git fetch --prune
 
     # push changes to mirror repo
-    git push ${GITHUB_REPO_URL} ${branch}
+    git push --all ${GITHUB_REPO_URL}
+    git push --tags ${GITHUB_REPO_URL}
 
     # change back to base-dir
     cd ..
